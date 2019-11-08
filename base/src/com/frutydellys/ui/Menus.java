e9ba16c201652ca82e2aff52433008f6cee8945f
cursojava <jefersonwillians137@gmail.com>
2019-11-08 16:58:56 -0400

    Classe Menus

--- base/src/com/frutydellys/ui/Menus.java
+++ base/src/com/frutydellys/ui/Menus.java
@@ -1,15 +1,16 @@
 package com.frutydellys.ui;
 
-
 import java.util.Scanner;
 
 public class Menus {
-    private int mp;
+    Scanner sc = new Scanner(System.in);
+    private int mp, mj;
     public void menuPrincipal() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("==========Frutydellys=========");
-        System.out.println(" [1] Iniciar");
-        System.out.println(" [2] Sair");
+        System.out.println("==============================");
+        System.out.println("|         FRUTYDELLYS        |");
+        System.out.println("==============================");
+        System.out.println("| [1] Iniciar jogo           |");
+        System.out.println("| [2] Sair do jogo           |");
         System.out.println("==============================");
         System.out.print(" >>> ");
         mp = sc.nextInt();
@@ -21,10 +22,31 @@ public class Menus {
     public int getMp() {
         return mp;
     }
-
     public void setMp(int mp) {
         this.mp = mp;
     }
+
+    public void menuInicioJogo() {
+        System.out.println("=======Iniciar jogo=======");
+        System.out.println("| [1] Jogar              |");
+        System.out.println("| [2] Mapa               |");
+        System.out.println("| [3] Ajuda              |");
+        System.out.println("| [4] Retornar           |");
+        System.out.println("==========================");
+        System.out.println(" >>> ");
+        mj = sc.nextInt();
+        while (mj < 1 || mj > 4) {
+            System.out.print(" >>> ");
+            mj = sc.nextInt();
+        }
+    }
+    public int getMj() {
+        return mj;
+    }
+    public void setMj(int mj) {
+        this.mj = mj;
+    }
+
 
 }
 
