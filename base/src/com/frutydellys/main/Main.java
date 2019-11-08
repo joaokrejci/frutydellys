05c57124a6d7325cc73bf156efd82ba9a80b120a
lolincoln <lincolnmamorim@hotmail.com>
2019-11-08 17:29:14 -0400

    locais

--- base/src/com/frutydellys/main/Main.java
+++ base/src/com/frutydellys/main/Main.java
@@ -2,11 +2,17 @@ package com.frutydellys.main;
 
 import com.frutydellys.maps.Locais;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
     }
     void initializelocals() {
-        //Locais Praca = new Locais("","","","","")
+        locais.put("Praça", new Locais("",null,null,null,null));
+
     }
+    public HashMap<String, Locais> locais = new HashMap<>();
+
+
 }
--- base/src/com/frutydellys/maps/Locais.java
+++ base/src/com/frutydellys/maps/Locais.java
@@ -1,13 +1,17 @@
 package com.frutydellys.maps;
 
+import com.frutydellys.characters.Inimigos;
+import com.frutydellys.characters.Npc;
+import com.frutydellys.items.Item;
+
 public class Locais {
     String descricao;
-    String itens[];
-    String inimigos[];
-    String npc[];
-    String saidas[];
+    Item itens[];
+    Inimigos inimigos[];
+    Npc npc[];
+    Locais saidas[];
 
-    public Locais(String descricao, String[] itens, String[] inimigos, String[] npc, String[] saidas) {
+    public Locais(String descricao, Item[] itens, Inimigos[] inimigos, Npc[] npc, Locais[] saidas) {
         this.descricao = descricao;
         this.itens = itens;
         this.inimigos = inimigos;
